93a14eaab1b77369128fa69f0d5331d8b9e10575
Zheyuu <joreeliu1996@gmail.com>
2020-06-18 23:22:55 -0400

    start project

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -12,6 +12,9 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
+from sqlalchemy.orm import relationship
+
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -20,6 +23,8 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 # TODO: connect to a local postgresql database
 
@@ -56,6 +61,16 @@ class Artist(db.Model):
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+  __tablename__ = 'Show'
+
+  id = db.Column(db.Integer, primary_key=True)
+  artisit_id = db.Column(db.Integer, unique=True)
+  venue_id = db.Column(db.Integer, unique=True)
+  start_time = db.Column(db.DateTime)
+
+  artisit = relationship('Artist', foreign_keys='Show.artisit_id')
+  venue = relationship('Venue', foreign_keys='Show.venue_id')
 
 #----------------------------------------------------------------------------#
 # Filters.
