27e20725ce445ef7e3468cb5187b6150c8c9c870
Zheyu(Joree) <zl2701@columbia.edu>
2020-06-24 15:51:36 -0400

    some auth changes

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -5,9 +5,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'zheyu.auth0.com'
+AUTH0_DOMAIN = 'zheyu.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'coffee'
+API_AUDIENCE = 'http://localhost:5000'
 
 ## AuthError Exception
 '''
--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: "http://127.0.0.1:5000", // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
-  }
+    url: "zheyu.us", // the auth0 domain prefix
+    audience: "http://localhost:5000", // the audience set for the auth0 app
+    clientId: "xkhN2MLbE0yDwRe4wESC8oOeNbkqLYJt", // the client id generated for the auth0 app
+    callbackURL: "http://localhost:8100", // the base url of the running ionic application.
+  },
 };
