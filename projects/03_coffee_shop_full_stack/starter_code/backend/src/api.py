a610309e229721f2fe5e988216cbe2a182ee54db
Zheyu(Joree) <zl2701@columbia.edu>
2020-06-24 22:08:28 -0400

    added error hander

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -95,7 +95,7 @@ def patch_drinks(jwt, drink_id):
     update_drink_data = json.loads(request.data.decode('utf-8'))
     update_drink = Drink.query.get(drink_id)
     new_drink = Drink(title=update_drink_data['title'], recipe=json.dumps(update_drink_data['recipe']))
-    Drink.UPDATE(update_drink)
+    new_drink.update()
     drinks = list(map(Drink.long, Drink.query.all()))
     result = {
         "success": True,
@@ -131,6 +131,7 @@ def delete_drinks(jwt, drink_id):
 '''
 Example error handling for unprocessable entity
 '''
+
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
@@ -138,17 +139,6 @@ def unprocessable(error):
                     "error": 422,
                     "message": "unprocessable"
                     }), 422
-
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False, 
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-
-'''
 
 '''
 @TODO implement error handler for 404
@@ -166,6 +156,15 @@ def not_found_error(error):
 @TODO implement error handler for AuthError
     error handler should conform to general task above 
 '''
+
+
+@app.errorhandler(401)
+def auth_error(error):
+    return jsonify({
+        'success': False,
+        'error': 401,
+        'message': 'Not authorized'
+    }), 401
 
 
 if __name__ == '__main__':
